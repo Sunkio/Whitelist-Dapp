238184f9deb63b160a9bd14a9bb4217466d6aed9
Tanja Schmidt <tanja@tanjasportfolio.com>
2023-01-13 09:00:32 +0100

    add Sepolia alert

--- wl-frontend/pages/index.js
+++ wl-frontend/pages/index.js
@@ -38,8 +38,8 @@ export default function Home() {
     // If user is not connected to the Goerli network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
     if (chainId !== 5) {
-      window.alert("Change the network to Goerli");
-      throw new Error("Change network to Goerli");
+      window.alert("Change the network to Sepolia");
+      throw new Error("Change network to Sepolia");
     }
 
     if (needSigner) {
